ce7f2672f30831eabf24ec67a4a54eff6d45da60
Asmaa <asmaa.aboasha@gmail.com>
2020-09-24 16:27:29 -0700

    Added GET method route for user addresses

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -75,4 +75,16 @@ module.exports = function(app) {
       });
     });
   });
+
+  app.get("/api/locations", (req, res) => {
+    db.User.findAll({
+      attributes: ["address"]
+    }).then( (data) => {
+      const addresses = data.map( (item) => { 
+        request(`https://maps.googleapis.com/maps/api/geocode/json?address=${item.dataValues.address}&key=YOUR_API_KEY`,(err, response, body) => {
+          console.log(response)
+        })
+      });
+    });
+  });
 };
